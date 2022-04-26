1423ab98bb80f7887e456ae7d172c52728f3238b
Igor Penkin <penkin.igor@gmail.com>
2022-04-26 14:26:10 +0300

    Getting basic display

--- Instafetcher/Helpers/APICaller.swift
+++ Instafetcher/Helpers/APICaller.swift
@@ -34,7 +34,7 @@ extension APICaller {
     
     func getMe() {
         guard let token = AuthManager.shared.accessToken,
-              let apiUrl = URL(string: Constants.baseAPIURL + "/me?" + "fields=id,username" + "&access_token=\(token)")
+              let apiUrl = URL(string: Constants.baseAPIURL + "/me?" + "fields=id,username,account_type,media_count" + "&access_token=\(token)")
         else { fatalError() }
         var request = URLRequest(url: apiUrl)
         request.httpMethod = HTTPMethod.GET.rawValue
@@ -48,6 +48,52 @@ extension APICaller {
                 let result = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
                 //JSONDecoder().decode(UserProfile.self, from: data)
                 Logger.log(object: Self.self, method: #function, message: "Got user profile model:", body: result)
+            } catch {
+                Logger.log(object: Self.self, method: #function, message: "Error:", body: error)
+            }
+        }
+        task.resume()
+    }
+    
+    func getMeMedia() {
+        guard let token = AuthManager.shared.accessToken,
+              let apiUrl = URL(string: Constants.baseAPIURL + "/me/media?" + "fields=id,username,caption,media_type,media_url,permalink,thumbnail_url,timestamp" + "&access_token=\(token)")
+        else { fatalError() }
+        var request = URLRequest(url: apiUrl)
+        request.httpMethod = HTTPMethod.GET.rawValue
+        request.timeoutInterval = 20
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                Logger.log(object: Self.self, method: #function, message: "Fail data")
+                return
+            }
+            do {
+                let result = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                //JSONDecoder().decode(UserProfile.self, from: data)
+                Logger.log(object: Self.self, method: #function, message: "Got media object:", body: result)
+            } catch {
+                Logger.log(object: Self.self, method: #function, message: "Error:", body: error)
+            }
+        }
+        task.resume()
+    }
+    
+    func getMedia(id: String) {
+        guard let token = AuthManager.shared.accessToken,
+              let apiUrl = URL(string: Constants.baseAPIURL + "/\(id)?" + "fields=id,username,caption,media_type,media_url,permalink,thumbnail_url,timestamp" + "&access_token=\(token)")
+        else { fatalError() }
+        var request = URLRequest(url: apiUrl)
+        request.httpMethod = HTTPMethod.GET.rawValue
+        request.timeoutInterval = 20
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                Logger.log(object: Self.self, method: #function, message: "Fail data")
+                return
+            }
+            do {
+                let result = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                //JSONDecoder().decode(UserProfile.self, from: data)
+                Logger.log(object: Self.self, method: #function, message: "Got media object:", body: result)
             } catch {
                 Logger.log(object: Self.self, method: #function, message: "Error:", body: error)
             }
--- Instafetcher/Screens/CaseOneViewController.swift
+++ Instafetcher/Screens/CaseOneViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 final class CaseOneViewController: UIViewController {
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Case One"
         view.backgroundColor = .secondarySystemBackground
         APICaller.shared.getMe()
+        APICaller.shared.getMeMedia()
+        APICaller.shared.getMedia(id: "17938814354046665")
     }
 }
